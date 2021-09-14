71f58852e92a25fc149659cbce0c6ca2e654e60c
evillt <ijoec123@gmail.com>
2021-09-14 18:54:38 +0800

    fix(tryOnMount): types

--- packages/shared/tryOnMount/index.ts
+++ packages/shared/tryOnMount/index.ts
@@ -2,14 +2,12 @@ import { onMount } from 'svelte'
 import { tryOnDestroy } from '../tryOnDestroy'
 import { Fn, noop, tryGetCurrentComponent } from '../utils'
 
-export type OnMountFn = Fn | (() => Fn)
-
 /**
- * Safe `onMount`. Call `onMount()` if it's inside a component lifecycle, if not, run just call the function.
+ * Call `onMount()` if it's inside a component lifecycle, if not, run just call the function.
  *
  * @param fn
  */
-export function tryOnMount(fn: OnMountFn) {
+export function tryOnMount(fn: Fn | (() => Fn)) {
   if (tryGetCurrentComponent()) {
     onMount(fn)
   } else {
