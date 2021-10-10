f1828153f865b7064cb86907997f70792547e96d
2nthony <ijoec123@gmail.com>
2021-10-10 17:18:51 +0800

    fix(whenever): improvement (#29)

--- packages/shared/whenever/index.ts
+++ packages/shared/whenever/index.ts
@@ -1,8 +1,19 @@
+import { toWritable } from '../utils'
+import { Fn, MaybeWritable } from '../utils/types'
+
 /**
  * Shorthand for watching value to be truthy
+ *
+ * @see https://svelte-use.vercel.app/shared/whenever
  */
-export function whenever<T>(source: T, cb: Function) {
-  if (source) {
-    cb()
-  }
+export function whenever<T>(source: MaybeWritable<T>, cb: Fn) {
+  const store = toWritable(source)
+  let ov: T | undefined
+
+  return store.subscribe((v) => {
+    if (v) {
+      cb(v, ov)
+      ov = v
+    }
+  })
 }
